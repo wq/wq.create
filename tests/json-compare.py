bf8d63be1d4ad93d7166ac91e0e3cfb295a09ca1
S. Andrew Sheppard <andrew@wq.io>
2018-04-24 21:35:19 +0000

    better comparison of unexpected values

--- tests/json-compare.py
+++ tests/json-compare.py
@@ -8,14 +8,17 @@ json1 = json.load(open(file1))
 json2 = json.load(open(file2))
 
 def unpack_diff(json1, json2, prefix=''):
-    if isinstance(json1, list):
+    if isinstance(json1, list) and isinstance(json2, list):
         for i, (val1, val2) in enumerate(zip(json1, json2)):
             unpack_diff(val1, val2, prefix + '[%s]' % i)
+        if len(json1) != len(json2):
+            print('len(%s)' % prefix, len(json1), '!=', len(json2))
 
-    elif isinstance(json1, dict):
+    elif isinstance(json1, dict) and isinstance(json2, dict):
         if prefix:
             prefix += '.'
-        for key, val1 in json1.items():
+        for key in sorted(set(json1.keys()) | set(json2.keys())):
+            val1 = json1.get(key)
             val2 = json2.get(key)
             unpack_diff(val1, val2, prefix + key)
 
